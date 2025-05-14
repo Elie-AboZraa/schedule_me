361d564580760999399f8f6db81c7333512a8e92
AGminuse <aghiadmuhlla@gmail.com>
2025-05-14 08:45:09 +0300

    adding plans , adding the lazy loader for Sceduals

---
+++ plan/Timetable.dart
@@ -0,0 +1,24 @@
+import 'Lecture.dart';
+import 'StudentPref.dart';
+
+class Timetable {
+  final List<Lecture> _lectures = [];
+
+  void addLecture(Lecture lecture) {
+    if (!_hasConflict(lecture)) {
+      _lectures.add(lecture);
+    }
+  }
+
+  bool _hasConflict(Lecture newLecture) {
+    return _lectures.any((lecture) => lecture.conflictsWith(newLecture));
+  }
+
+  bool isValid(StudentPref prefs) {
+    final days = _lectures.map((l) => l.day).toSet().length;
+    return days <= prefs.maxDays;
+  }
+
+  // Public getter
+  List<Lecture> get lectures => List.unmodifiable(_lectures);
+}
